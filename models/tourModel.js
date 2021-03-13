c18785be83454b6922b060e40380e2bf80cac8fe
Vitaliy Vasylyk <fygetoid@gmail.com>
2021-03-13 23:12:31 +0200

    Document, Query, Aggregation = Middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slufify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true, // Remove white space
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -53,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false, // It will never send this "createdAt" to the client
     },
     startDates: [Date], // '2021-03-21' again Mongodb will try to parse it
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toObject: { virtuals: true },
@@ -66,6 +72,47 @@ tourSchema.virtual('halfGroupSize').get(function () {
 
 tourSchema.virtual('durationWeeks').get(function () {
   return (this.duration / 7).toFixed(1);
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create() NOT runs when .insertMany()
+tourSchema.pre('save', function (next) {
+  // this = currently proccessed document
+  // pre save hook// hook it "save" || pre save middleware
+  this.slug = slufify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// // Post Document Middleware functions are executed after all .pre() middleware
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.startTime = Date.now();
+  next();
+});
+
+// Runs after the query has already executed, docs = all documents in the query
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.startTime} milliseconds`);
+
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
