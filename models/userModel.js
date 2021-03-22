24a2003bdf3951394c968b1b5250bc0b4cb6fd47
Vitaliy Vasylyk <fygetoid@gmail.com>
2021-03-23 00:39:01 +0200

    Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+// const bcrypt = require('bcryptjs');
+const argon2 = require('argon2');
 
 // username, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -29,7 +31,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same, please try again',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  // this.password = await bcrypt.hash(this.password, 12);
+  this.password = await argon2.hash(this.password);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
